f6eaeebdf9ec13067f45dca50a7427773071ff98
abdelfattah <abdelfatahwenny@yahoo.com>
2023-08-08 14:08:08 -0700

    function to make string uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            num = 32
+        else:
+            num = 0
+        if i == len(str) - 1:
+            print("{:c}".format(ord(str[i]) - num))
+        else:
+            print("{:c}".format(ord(str[i]) - num), end='')
+
+uppercase("mynameISwenny")
