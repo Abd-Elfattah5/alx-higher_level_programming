b154abf18c83ba7303c961e3b28a964351e9417c
abdelfattah <abdelfatahwenny@yahoo.com>
2023-08-08 12:47:17 -0700

    followd the coding style and solved the negative problem

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-remain = nubmer % 10
+remain = abs(number) % 10
+if number < 0:
+    remain = remain * -1
 if remain > 5:
-    print(f"last digit of {number} is {remain} and is greater than 5")
-elif remain < 6 and remain not 0:
-    print(f"last digit of {number} is {remain} and is less than 6 and not 0")
-else
-print(f"last digit of {number} is {remain} and is 0")
+    print("last digit of {} is {} and\
+ is greater than 5".format(number, remain))
+elif remain < 6 and remain != 0:
+    print("last digit of {} is {} and\
+ is less than 6 and not 0".format(number, remain))
+else:
+    print("last digit of {} is {} and is 0".format(number, remain))
