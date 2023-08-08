9c23ebefb1de643c3bb4840bf738bc828d1291f7
abdelfattah <abdelfatahwenny@yahoo.com>
2023-08-08 15:03:17 -0700

    solved the problem of adding a new node to a linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * insert_node - function to insert a node in a sorted linked list
+ * Return: the address of the new node or NULL if it fails
+ * @head: the start of the linked list
+ * @number: the number to put in the node
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp_new, *temp;
+
+	if (head == NULL)
+		return (NULL);
+	temp_new = malloc(sizeof(listint_t));
+
+	if (temp_new == NULL)
+		return (NULL);
+	temp = *head;
+	temp_new->n = number;
+
+	while (temp != NULL)
+	{
+		if (temp->n > temp_new->n)
+		{
+			temp_new->next = temp;
+			return (temp_new);
+		}
+		else if (temp->n <= temp_new->n && temp->next == NULL)
+		{
+			temp->next = temp_new;
+			temp_new->next = NULL;
+			return (temp_new);
+		}
+		else if (temp->n <= temp_new->n && temp->next->n >= temp_new->n)
+		{
+			temp_new->next = temp->next;
+			temp->next = temp_new;
+			return (temp_new);
+		}
+		temp = temp->next;
+	}
+	return (NULL);
+}
