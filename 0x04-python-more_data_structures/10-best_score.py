cea8d91f299e38439802781c6ff0368bef21945a
abdelfattah <abdelfatahwenny@yahoo.com>
2023-08-16 17:30:26 -0700

    finding the biggest score in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None or len(a_dictionary) == 0:
+        return (None)
+
+    key = (list(a_dictionary))[0]
+    value = a_dictionary[key]
+
+    for i in a_dictionary:
+        if a_dictionary[i] > a_dictionary[key]:
+            key = i
+            value = a_dictionary[key]
+    return (key)
