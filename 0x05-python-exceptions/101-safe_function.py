26dfcaac93e245dadb6b336642b7674af4e5b667
abdelfattah <abdelfatahwenny@yahoo.com>
2023-08-28 17:15:18 -0700

    trying to fix pycode error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,6 +7,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return (fct(*args))
-    except:
+    except (TypeError, ValueError, ZeroDivisionError, IndexError):
         sys.stderr.write("Exception: {}\n".format(sys.exc_info()[1]))
         return (None)
