9bef74f1532a4af8886f6f41fd8cc757b6fed090
abdelfattah <abdelfatahwenny@yahoo.com>
2023-08-29 08:01:19 -0700

    fixed pycodestyle

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -21,6 +21,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """ calculating square area
             Return (type: int): the area of the square"""
