a16e22192d408be8e6c7ce4b8db06038e0d2fa14
abdelfattah <abdelfatahwenny@yahoo.com>
2023-08-29 07:39:47 -0700

    new class attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines a class of square"""
+
+class Square:
+    """
+        Square: class to define square
+        Attributes:
+            size (no type or value restrictions): size of square
+        Method:
+            __init__: initializing the class attributes
+    """
+    def __init__(self, size):
+        """ initialize the instance attributes
+        Args:
+            size (no type): size of the square
+        """
+        self.__size = size
