b335d4f79767fd18a7a9ab48ca2cd6d0ad02e3f3
abdelfattah <abdelfatahwenny@yahoo.com>
2023-08-29 07:23:45 -0700

    trying to follow google documenting

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
+""" defining my first class """
 class Square:
-    """this is a Square class to do calculations
-    """
+    """this is a Square class to do calculations"""
     pass
