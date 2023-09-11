7297e9f7a036bfeda662dbb17cce102a63741c95
abdelfattah <abdelfatahwenny@yahoo.com>
2023-09-11 16:49:20 -0700

    added readme file and function to return an object attribute

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """ function to return all the object attributes
+        
+        Args:
+            obj (unknown): some object to retreive its attributes
+
+        Return:
+            dictionary: all the object attributes
+    """
+    return (obj.__dict__)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+learning iheritance concepts in python
