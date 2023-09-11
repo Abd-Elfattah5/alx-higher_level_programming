22ffff9f43982472d5af6bc4594d85c02f0f768a
abdelfattah <abdelfatahwenny@yahoo.com>
2023-09-11 16:53:43 -0700

    tried to do more documenting

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
+"""this a lookup module """
 def lookup(obj):
     """ function to return all the object attributes
 
         Args:
-            obj (unknown): some object to retreive its attributes
+           - obj (unknown): some object to retreive its attributes
 
         Return:
             dictionary: all the object attributes
     """
-    return (obj.__dict__)
+    return (obj)
