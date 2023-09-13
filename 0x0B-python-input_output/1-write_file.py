13d13050ea18e62468a8b6028d4e19c12454f620
abdelfattah <abdelfatahwenny@yahoo.com>
2023-09-13 15:54:19 -0700

    overwriting files

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a module to write a text to file and overwrite the file"""
+
+
+def write_file(filename="", text=""):
+    """functions that overwrites a file with a text
+
+        Args:
+            filename (str): the file to overwrite
+            text (str): the message to write
+    """
+    with open(filename, 'w', encoding='utf-8') as my_file:
+        my_file.write(text)
