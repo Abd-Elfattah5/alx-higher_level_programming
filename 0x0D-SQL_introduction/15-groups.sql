4a6427b88ae1e294b51accb855485b084fc7469b
abdelfattah <abdelfatahwenny@yahoo.com>
2023-10-17 18:24:14 -0700

    fixing the order

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- counting the same score records
-SELECT score, COUNT(score) AS number FROM second_table ORDER BY score DESC GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
