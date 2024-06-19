969bdb8eb987abbef7240ddc7e7e7e03ae573a9d
wenny <abdelfatahwenny@yahoo.com>
2024-06-18 19:28:20 -0700

    square drawing with loops

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arg1 = Number(process.argv[2]);
+if (isNaN(arg1)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < arg1; i++) {
+    let row = '';
+    for (let j = 0; j < arg1; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
