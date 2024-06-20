1981613752f8ccc9d316cefd7a251cfc4e038988
wenny <abdelfatahwenny@yahoo.com>
2024-06-20 09:54:24 -0700

    removed the class Rectangle

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,33 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let star = '';
-      for (let j = 0; j < this.width; j++) star += 'X';
-      console.log(star);
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-}
-
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends require('./5-square.js') {
   constructor (size) {
     super(size, size);
   }
