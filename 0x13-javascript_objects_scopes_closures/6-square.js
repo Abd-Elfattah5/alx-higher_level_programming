78ad01e953942bd7212355ecfbfcc0b21fe8fdad
wenny <abdelfatahwenny@yahoo.com>
2024-06-20 09:49:14 -0700

    added a function to print with costume char

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,46 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let star = '';
+      for (let j = 0; j < this.width; j++) star += 'X';
+      console.log(star);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        let star = '';
+        for (let j = 0; j < this.width; j++) star += c;
+        console.log(star);
+      }
+    } else {
+      super.print();
+    }
+  }
+};
