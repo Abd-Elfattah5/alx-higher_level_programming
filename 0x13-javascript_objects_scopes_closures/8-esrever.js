b482632f4220fc414609da02c9c38cd781ebdbec
wenny <abdelfatahwenny@yahoo.com>
2024-06-20 10:53:12 -0700

    changed the function ans used reduction

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const revlist = [];
-  for (let i = list.length ; i > 0; i--) {
-    revlist.push(list[i - 1]);
-  }
-  return list;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
