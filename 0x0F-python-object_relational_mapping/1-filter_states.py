2dfd77e1a4f6da78e38345a3ea01478996071427
wenny <abdelfatahwenny@yahoo.com>
2024-06-22 06:15:49 -0700

    made a query to get the states with names starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""this is a module to execute simple queries and test MySQLdb module"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(user=username, passwd=password, db=database_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE SUBSTRING(name, 1, 1) = 'N' ORDER BY id ASC")
+
+    for e in cur.fetchall():
+        print(e)
+    cur.close()
+    db.close()
