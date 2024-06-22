08d9d4e36c26041d70729c9fe00635ab1e8c6abf
wenny <abdelfatahwenny@yahoo.com>
2024-06-22 06:28:54 -0700

    made a script to check for matching user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""this is a module to execute simple queries and test MySQLdb module"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(user=username, passwd=password, db=database_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                WHERE name = %s \
+                ORDER BY id ASC", (sys.argv[4],))
+
+    for e in cur.fetchall():
+        print(e)
+    cur.close()
+    db.close()
