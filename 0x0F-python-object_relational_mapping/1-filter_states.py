0bc33d55e3a7527c53b0f9c6768379812e391f93
wenny <abdelfatahwenny@yahoo.com>
2024-06-22 06:25:10 -0700

    removed the = sign

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(user=username, passwd=password, db=database_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states\
-                WHERE name LIKE BINARY = 'N%'\
+    cur.execute("SELECT * FROM states \
+                WHERE name LIKE BINARY 'N%' \
                 ORDER BY id ASC")
 
     for e in cur.fetchall():
