377400ae27a5f4545dbe9bb4186d6e87e9df2ad4
wenny <abdelfatahwenny@yahoo.com>
2024-06-22 06:05:27 -0700

    created and activated an environment, imported and used the mysqldb module, and executed some quries

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""this is a module to execute simple queries and test MySQLdb module"""
+
+import sys
+import MySQLdb
+
+username = sys.argv[1]
+password = sys.argv[2]
+database_name = sys.argv[3]
+
+db = MySQLdb.connect(user=username, passwd=password, db=database_name)
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+for e in cur.fetchall():
+    print(e)
