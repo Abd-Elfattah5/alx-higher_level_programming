aad533eddb00f2b1c4b77b12e31bd443e65ae43d
wenny <abdelfatahwenny@yahoo.com>
2024-06-28 16:50:40 -0700

    get the id of the desired state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" using sqlalchemy to query date """
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    states = session.query(State).filter(State.name == argv[4])\
+                                 .order_by(State.id).first()
+    if state is not None:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+    session.close()
