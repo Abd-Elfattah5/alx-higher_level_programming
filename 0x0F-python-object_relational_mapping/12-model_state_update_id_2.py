328b5410a4523713f3295351dcc7704de1315c9e
wenny <abdelfatahwenny@yahoo.com>
2024-06-28 17:05:08 -0700

    filter_by not filter

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     session = Session()
     Base.metadata.create_all(engine)
 
-    state = session.query(State).filter(id='2').first()
+    state = session.query(State).filter_by(id='2').first()
     state.name = "New Mexico"
     session.commit()
     session.close()
