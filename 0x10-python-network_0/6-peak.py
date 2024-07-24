75c606beabfe46a4f447a79f1a509e1371d39d3e
wenny <abdelfatahwenny@yahoo.com>
2024-07-24 09:12:44 -0700

    finding the peak with a better way

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
+""" script for finding the peak of a list """
 
-
-def find_peak(list_of_integers):
-
-    if list_of_integers is None or len(list_of_integers) == 0:
+def find_peak(nums):
+    """
+        a function to find a peak of a list by iterating over all the elements
+    """
+    if not isinstance(nums, list) or len(nums) == 0:
         return None
+    peak = nums[0]
 
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-
-    mid_idx = int(len(list_of_integers) / 2)
-
-    if mid_idx != len(list_of_integers) - 1:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
-           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-    else:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-        else:
-            return list_of_integers[mid_idx - 1]
-
-    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
-        a_list = list_of_integers[0:mid_idx]
-    else:
-        a_list = list_of_integers[mid_idx + 1:]
-
-    return find_peak(a_list)
+    for n in nums:
+        if n > peak:
+            peak = n
+    return peak
