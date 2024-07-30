2f1a9484b5da7b4675919bd7f719c5f76b970b44
wenny <abdelfatahwenny@yahoo.com>
2024-07-30 15:47:42 -0700

    used another url to access the movies

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,22 @@
 #!/usr/bin/node
 
-const url = 'https://swapi-api.alx-tools.com/api/people/18';
+const url = process.argv[2];
 const request = require('request');
 
 request(url, (err, response, body) => {
   if (!err && response.statusCode === 200) {
     const info = JSON.parse(body);
-    const movies = info.films.length;
-    console.log(movies);
+    const movies = info.results;
+    let num = 0;
+    movies.forEach((movie) => {
+      const actors = movie.characters;
+      actors.forEach((actor) => {
+        if (actor === 'https://swapi-api.alx-tools.com/api/people/18/') {
+          num += 1;
+        }
+      });
+    });
+    console.log(num);
   } else {
     console.log(err);
   }
